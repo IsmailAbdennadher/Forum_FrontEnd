9bb01962d66b6fb3ea9d91dab241492db2fd5dab
Abdennadher Ismail <ismail.abdennadher@aymax.fr>
2021-03-27 17:14:50 +0100

    set login page, refactor code

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-login',
@@ -10,28 +11,28 @@ export class LoginComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.loadScripts();
     console.log('login component');
     document.body.className = "home-page bp-nouveau home page-template page-template-page-templates page-template-full-width page-template-page-templatesfull-width-php page page-id-95 theme-beehive woocommerce-no-js beehive beehive-guest-user beehive-child elementor-default elementor-kit-588 elementor-page elementor-page-95 desktop-slidenav full-width no-js";
-    /* this.createLink('https://mythemestore.com/beehive-preview/wp-content/uploads/2020/01/cropped-site-icon-192x192.png'); */
-    this.loadScript('assets/wp-includes/js/plupload/moxie.mine34c.js?ver=1.3.5');
-    this.loadScript('https://mythemestore.com/beehive-preview/wp-includes/js/plupload/plupload.min.js?ver=2.1.9');
+    let t = document.body.querySelector("app-login")?.innerHTML;
+     /*  console.log(document.body.getElementsByTagName("app-login")?.item(0)?.replaceWith(""));
+      console.log(document.body.getElementsByTagName("app-root")?.item(0)?.replaceWith(""));
+  
+    if(t != undefined)
+      {
+        let div = document.createElement('div');
+        div.innerHTML = t;
+        div.id = "beehive-page";
+        div.className = "site";
+        document.body.appendChild(div);
+      } */
+    /* this.loadScript('https://mythemestore.com/beehive-preview/wp-includes/js/plupload/plupload.min.js?ver=2.1.9');
     this.loadScript('https://mythemestore.com/beehive-preview/wp-content/plugins/elementor/assets/lib/swiper/swiper.min.js?ver=5.3.6');
-    this.loadScript('https://mythemestore.com/beehive-preview/wp-content/themes/beehive/assets/js/beehive-elements.min.js?ver=1.3.5.1');
-    /* this.loadScript('https://mythemestore.com/beehive-preview/wp-content/plugins/elementor/assets/js/frontend-modules.min.js?ver=3.0.15'); */
-    this.loadScript('https://mythemestore.com/beehive-preview/wp-includes/js/jquery/ui/position.min.js?ver=1.11.4');
-    this.loadScript('https://mythemestore.com/beehive-preview/wp-content/plugins/elementor/assets/lib/dialog/dialog.min.js?ver=4.8.1');
-    this.loadScript('https://mythemestore.com/beehive-preview/wp-content/plugins/elementor/assets/lib/waypoints/waypoints.min.js?ver=4.0.2');
-    this.loadScript('https://mythemestore.com/beehive-preview/wp-content/plugins/elementor/assets/lib/share-link/share-link.min.js?ver=3.0.15');
-/*     this.loadScript('https://mythemestore.com/beehive-preview/wp-content/plugins/elementor/assets/js/frontend.min.js?ver=3.0.15'); */
-/*     this.loadScript('https://mythemestore.com/beehive-preview/wp-includes/js/jquery/jquery.js?ver=1.12.4-wp');
-    this.loadScript('https://mythemestore.com/beehive-preview/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.1'); 
-    this.loadScript('https://mythemestore.com/beehive-preview/wp-content/plugins/buddypress/bp-core/js/widget-members.min.js?ver=7.1.0');
-    this.loadScript('https://mythemestore.com/beehive-preview/wp-content/plugins/buddypress/bp-core/js/jquery-query.min.js?ver=7.1.0');
-    this.loadScript('https://mythemestore.com/beehive-preview/wp-content/plugins/buddypress/bp-core/js/vendor/jquery-cookie.min.js?ver=7.1.0');
-    this.loadScript('https://mythemestore.com/beehive-preview/wp-content/plugins/buddypress/bp-core/js/vendor/jquery-scroll-to.min.js?ver=7.1.0');
-    this.loadScript('https://mythemestore.com/beehive-preview/wp-includes/js/underscore.min.js?ver=1.8.3');
-    this.loadScript('https://mythemestore.com/beehive-preview/wp-includes/js/backbone.min.js?ver=1.4.0');
-    this.loadScript('https://mythemestore.com/beehive-preview/wp-content/plugins/buddypress-media/lib/media-element/wp-mediaelement.min.js?ver=4.6.6');*/
+    this.loadScript('https://mythemestore.com/beehive-preview/wp-content/themes/beehive/assets/js/beehive-elements.min.js?ver=1.3.5.1'); */
+   /*  this.loadScript('assets/wp-includes/js/jquery/ui/position.mine899.js?ver=1.11.4');
+    this.loadScript('assets/wp-content/plugins/elementor/assets/lib/dialog/dialog.mina288.js?ver=4.8.1');
+    this.loadScript('assets/wp-content/plugins/elementor/assets/lib/waypoints/waypoints.min05da.js?ver=4.0.2');
+    this.loadScript('assets/wp-content/plugins/elementor/assets/lib/share-link/share-link.minc3cf.js?ver=3.0.15'); */
   }
 
   public loadScript(url : string) {
@@ -39,14 +40,19 @@ export class LoginComponent implements OnInit {
     let script = document.createElement('script');
     script.innerHTML = '';
     script.src = url;
-    script.async = true;
-    script.defer = true;
+    script.type = 'text/javascript'; 
     body.appendChild(script);
 }
-createLink(url: string, rel: string) {
-  let link: HTMLLinkElement = document.createElement('link');
-  link.setAttribute('rel', rel);
-  document.head.appendChild(link);
-  link.setAttribute('href', url);
-}
+loadScripts() { 
+  
+  // This array contains all the files/CDNs 
+  const dynamicScripts = environment.scripts;
+  for (let i = 0; i < dynamicScripts.length; i++) { 
+    const node = document.createElement('script'); 
+    node.src = dynamicScripts[i]; 
+    node.type = 'text/javascript'; 
+    node.async = false; 
+    document.getElementsByTagName('head')[0].appendChild(node); 
+  } } 
+
 }
